65d0571ef7bbe79f0af404bfb8abd1b39c0385ae
shash <shash@192.168.0.13>
2016-12-01 22:59:57 -0500

    shashi commit java project

---
+++ test/src/test/hello.java
@@ -0,0 +1,24 @@
+package test;
+
+public class hello {
+
+	public static void main(String[] args)
+	{
+System.out.println("hello");
+	}
+class A{
+
+	public void main(String[] args)
+	{
+	System.out.println("mello");
+	}
+	}
+class B{
+
+	public void main(String[] args)
+	{
+	System.out.println("jello");
+	}
+}
+
+}
